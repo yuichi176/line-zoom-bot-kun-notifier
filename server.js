f17f7fc8ae9ee362513546a24b1f6852d8b62b4d
Yuichi Sugiyama <ozwald176@gmail.com>
2023-09-22 18:36:44 +0900

    add firestore logic, error handling

--- server.js
+++ server.js
@@ -29,22 +29,67 @@ app.use('/message', express.urlencoded({ extended: true }));
 
 // Routing
 app.post('/message', (req, res) => {
-    handlePushMessage(req.body.to, req.body.messages)
-        .then((result) => res.json(result));
+    handlePushMessage(req.body)
+        .then((result) => res.json(result))
+        .catch((error) => {
+            if (error instanceof NotFoundError) {
+                res.status(404).json({ error: 'Not Found', message: error.message });
+            } else {
+                console.error(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        })
 });
 
 // Handler
-async function handlePushMessage(to, messages) {
-    console.log(`Push message to ${to}: ${messages}`)
+async function handlePushMessage(reqBody) {
+    const destination = reqBody.destination
+    const datetime = reqBody.datetime
+    const zoomUrl = reqBody.zoomUrl
     try {
+        const docRef = db.collection('destinations').doc(destination).collection('meetings').doc(datetime)
+        const doc = await docRef.get()
+        if (doc.exists === false) {
+            console.error(`${destination}:${datetime} is not exist`)
+            throw new NotFoundError('Meeting not found');
+        } else if (doc.data().isCancelled === true) {
+            console.error(`${destination}:${datetime} is cancelled`)
+            throw new NotFoundError('Meeting is cancelled');
+        }
+        // update isNotified true
+        await docRef.set({
+            startDatetime: doc.data().startDatetime,
+            zoomUrl: doc.data().zoomUrl,
+            isCancelled: doc.data().isCancelled,
+            isNotified: true,
+        })
+
         // Send push message
         // API Reference: https://developers.line.biz/ja/reference/messaging-api/#send-push-message
-        return client.pushMessage(to, messages)
+        console.log(`Push message to ${destination}: ${datetime}`)
+        return client.pushMessage(destination, [
+            {
+                "type": "text",
+                "text": "ミーティングの時間だよ"
+            },
+            {
+                "type": "text",
+                "text": zoomUrl
+            }
+        ])
     } catch (error) {
-        return Promise.resolve(null);
+        console.error(error)
+        throw error
     }
 }
 
 app.listen(process.env.PORT, () => {
     console.log(`express server listening on port ${process.env.PORT}`);
 });
+
+class NotFoundError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'NotFoundError';
+    }
+}
